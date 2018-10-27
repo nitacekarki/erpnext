da293a6cd36bf12f421f15b95f1a48435f7fcf6d
monroy95 <m.monroyc22@gmail.com>
2018-10-27 17:43:28 -0600

    Creation of child table crop_harvest_item_viability_window

---
+++ erpnext/agriculture/doctype/crop_harvest_item_viability_window/crop_harvest_item_viability_window.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2018, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Crop Harvest Item Viability Window', {
+	refresh: function(frm) {
+
+	}
+});
