f940827f876fc24e245dc1b2128b9c58acc93fdd
monroy95 <m.monroyc22@gmail.com>
2018-10-29 18:36:05 -0600

    Adding new `crop_cycle` field to Doctypes :
    Sales Order, Delivery Note, Sales Invoice, Stock Entry to link it to the Crop Cycle dashboard

--- erpnext/agriculture/doctype/crop_cycle/crop_cycle_dashboard.py
+++ erpnext/agriculture/doctype/crop_cycle/crop_cycle_dashboard.py
@@ -4,7 +4,7 @@ def get_data():
 	return {
 		'heatmap': True,
 		'heatmap_message': _('This is based on the Time Sheets created against this Crop Cycle'),
-		'fieldname': 'project', # Este campo debe existir en todas las transacciones como link field con el mismo nombre, para que desde el dashboard se puede referir a los demas
+		# 'fieldname': 'crop_cycle', # Este campo debe existir en todas las transacciones como link field con el mismo nombre, para que desde el dashboard se puede referir a los demas
 		'transactions': [
 			{
 				'label': _('Crop Cycle'),
