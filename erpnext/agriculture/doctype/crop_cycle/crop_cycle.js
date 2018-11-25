6032c3ed5c9c4e52a0baf8d288fca687ce8a7ab6
monroy95 <m.monroyc22@gmail.com>
2018-11-24 21:51:41 -0600

    Adapting Project Performance to Crop Cycle

--- erpnext/agriculture/doctype/crop_cycle/crop_cycle.js
+++ erpnext/agriculture/doctype/crop_cycle/crop_cycle.js
@@ -9,13 +9,13 @@ frappe.ui.form.on('Crop Cycle', {
 		frappe.realtime.on("List of Linked Docs", (output) => {
 			let analysis_doctypes = ['Soil Texture', 'Plant Analysis', 'Soil Analysis'];
 			let analysis_doctypes_docs = ['soil_texture', 'plant_analysis', 'soil_analysis'];
-			let obj_to_append = {soil_analysis: [], soil_texture: [], plant_analysis: []};
-			output['Location'].forEach( (land_doc) => {
-				analysis_doctypes.forEach( (doctype) => {
-					output[doctype].forEach( (analysis_doc) => {
+			let obj_to_append = { soil_analysis: [], soil_texture: [], plant_analysis: [] };
+			output['Location'].forEach((land_doc) => {
+				analysis_doctypes.forEach((doctype) => {
+					output[doctype].forEach((analysis_doc) => {
 						let point_to_be_tested = JSON.parse(analysis_doc.location).features[0].geometry.coordinates;
 						let poly_of_land = JSON.parse(land_doc.location).features[0].geometry.coordinates[0];
-						if (is_in_land_unit(point_to_be_tested, poly_of_land)){
+						if (is_in_land_unit(point_to_be_tested, poly_of_land)) {
 							obj_to_append[analysis_doctypes_docs[analysis_doctypes.indexOf(doctype)]].push(analysis_doc.name);
 						}
 					});
@@ -25,6 +25,106 @@ frappe.ui.form.on('Crop Cycle', {
 				obj_to_append: obj_to_append
 			});
 		});
+
+		if (frappe.model.can_read("Task")) {
+			frm.add_custom_button(__("Gantt Chart"), function () {
+				frappe.route_options = {
+					"crop_cycle": frm.doc.name
+				};
+				frappe.set_route("List", "Task", "Gantt");
+			});
+
+			frm.add_custom_button(__("Kanban Board"), () => {
+				frappe.call('erpnext.agriculture.doctype.crop_cycle.crop_cycle.create_kanban_board_if_not_exists', {
+					crop_cycle: frm.doc.name
+				}).then(() => {
+					frappe.set_route('List', 'Task', 'Kanban', frm.doc.name);
+				});
+			});
+		}
+
+	},
+	tasks_refresh: function (frm) {
+		var grid = frm.get_fields('tasks').grid;
+		grid.wrapper.find('select[data-fieldname="status"]').each(function () {
+			if ($(this).val() === 'Open') {
+				$(this).addClass('input-indicator-open');
+			} else {
+				$(this).removeClass('input-indicator-open');
+			}
+		});
+	},
+	linked_location: function (frm) {
+		if (frm.doc.linked_location && (frm.doc.planting_units_crop_cycle == 'Per Area')) {
+			frappe.db.get_value("Location", frm.doc.linked_location, "area")
+				.then((r) => {
+					if (r.message.area == 0) {
+						frappe.throw(__(`The location 
+											<a href= '#Form/Location/${frm.doc.linked_location}'><b>${frm.doc.linked_location}</b></a> 
+											you have selected does not have a selected area.`));
+					}
+				});
+		}
+	}
+});
+
+frappe.ui.form.on("Crop Cycle Task", {
+	edit_task: function (frm, doctype, name) {
+		var doc = frappe.get_doc(doctype, name);
+		if (doc.task_id) {
+			frappe.set_route("Form", "Task", doc.task_id);
+		} else {
+			frappe.msgprint(__("Save the document first."));
+		}
+	},
+
+	edit_timesheet: function (frm, cdt, cdn) {
+		var child = locals[cdt][cdn];
+		frappe.route_options = { "crop_cycle": frm.doc.name, "task": child.task_id };
+		frappe.set_route("List", "Timesheet");
+	},
+
+	make_timesheet: function (frm, cdt, cdn) {
+		var child = locals[cdt][cdn];
+		frappe.model.with_doctype('Timesheet', function () {
+			var doc = frappe.model.get_new_doc('Timesheet');
+			var row = frappe.model.add_child(doc, 'time_logs');
+			row.crop_cycle = frm.doc.name;
+			row.task = child.task_id;
+			frappe.set_route('Form', doc.doctype, doc.name);
+		})
+	},
+
+	status: function (frm, doctype, name) {
+		frm.trigger('tasks_refresh');
+	},
+});
+
+frappe.ui.form.on("Crop Cycle Harvest Item", {
+	view_stock_entry: function (frm, cdt, cdn) {
+		var child = locals[cdt][cdn];
+		frappe.route_options = { "crop_cycle": frm.doc.name };
+		frappe.set_route("List", "Stock Entry");
+	},
+	make_stock_entry: function (frm, cdt, cdn) {
+		var child = locals[cdt][cdn];
+		frappe.model.with_doctype('Stock Entry', function () {
+			var doc = frappe.model.get_new_doc('Stock Entry');
+			doc.crop_cycle = frm.doc.name;
+			doc.from_warehouse = frm.doc.linked_warehouse;
+			doc.purpose = 'Material Receipt';
+			doc.posting_date = child.expected_harvest_viability_date;
+			var row = frappe.model.add_child(doc, 'items');
+			row.item_code = child.item_code;
+			row.qty = child.qty;
+			row.uom = child.uom;
+			row.t_warehouse = child.target_warehouse;
+			row.basic_rate = child.valuation_rate;
+			row.conversion_factor = 1; // FIXME:
+			row.transfer_qty = child.qty; //FIXME:
+			row.valuation_rate = child.valuation_rate; //FIXME:
+			frappe.set_route('Form', doc.doctype, doc.name);
+		})
 	}
 });
 
--- erpnext/agriculture/doctype/crop_cycle/crop_cycle.py
+++ erpnext/agriculture/doctype/crop_cycle/crop_cycle.py
@@ -9,19 +9,35 @@ import ast
 import frappe
 from frappe import _
 from frappe.model.document import Document
-from frappe.utils import add_days
+from frappe.utils import add_days, getdate
 
 
 class CropCycle(Document):
 	def validate(self):
 		self.set_missing_values()
+		self.load_tasks()
+		self.load_crop_inputs()
+		self.load_crop_harvest_items()
+		self.validate_dates()
+		self.validate_creation()
+	
+	def onload(self):
+		"""Load crop cycle tasks for quick view"""
+		self.load_tasks()
+		self.load_crop_inputs()
+		self.load_crop_harvest_items()
 
 	def after_insert(self):
-		self.create_crop_cycle_project()
+		self.create_crop_cycle_tasks()
 		self.create_tasks_for_diseases()
 
 	def on_update(self):
 		self.create_tasks_for_diseases()
+		self.delete_task()
+		self.load_tasks()
+		self.load_crop_inputs()
+		self.load_crop_harvest_items()
+		self.validate_creation()
 
 	def set_missing_values(self):
 		crop = frappe.get_doc('Crop', self.crop)
@@ -32,11 +48,22 @@ class CropCycle(Document):
 		if not self.row_spacing_uom:
 			self.row_spacing_uom = crop.row_spacing_uom
 
-	def create_crop_cycle_project(self):
-		crop = frappe.get_doc('Crop', self.crop)
+	def validate_dates(self):
+		if self.start_date and self.end_date:
+			if getdate(self.end_date) < getdate(self.start_date):
+				frappe.throw(_("Expected End Date can not be less than Expected Start Date"))
 
-		self.project = self.create_project(crop.period, crop.agriculture_task)
-		self.create_task(crop.agriculture_task, self.project, self.start_date)
+	def validate_creation(self):
+		for d in frappe.get_all('Crop Cycle',
+			fields=['end_date', 'name', 'linked_location'],
+			filters={'docstatus': 1, 'linked_location': self.linked_location}):
+			if (getdate(self.start_date) <= d.end_date):
+				frappe.throw(_('''There is already a crop cycle <a href= "#Form/Crop Cycle/{0}"><b>{0}</b></a>
+				 				within the selected date range, please add one more day to the start date.''').format(d.name))
+
+	def create_crop_cycle_tasks(self):
+		crop = frappe.get_doc('Crop', self.crop)
+		self.create_task(crop.agriculture_task, self.title, self.start_date)
 
 	def create_tasks_for_diseases(self):
 		for disease in self.detected_disease:
@@ -50,28 +77,116 @@ class CropCycle(Document):
 		disease_doc = frappe.get_doc('Disease', disease)
 		self.create_task(disease_doc.treatment_task, self.name, start_date)
 
-	def create_project(self, period, crop_tasks):
-		project = frappe.new_doc("Project")
-		project.update({
-			"project_name": self.title,
-			"expected_start_date": self.start_date,
-			"expected_end_date": add_days(self.start_date, period - 1)
-		})
-		project.insert()
-
-		return project.name
-
-	def create_task(self, crop_tasks, project_name, start_date):
+	def create_task(self, crop_tasks, crop_cycle_name, start_date):
 		for crop_task in crop_tasks:
 			task = frappe.new_doc("Task")
 			task.update({
 				"subject": crop_task.get("task_name"),
 				"priority": crop_task.get("priority"),
-				"project": project_name,
-				"exp_start_date": add_days(start_date, crop_task.get("start_day") - 1),
-				"exp_end_date": add_days(start_date, crop_task.get("end_day") - 1)
+				"crop_cycle": crop_cycle_name,
+				"exp_start_date": add_days(start_date, crop_task.get("start_day")),
+				"exp_end_date": add_days(start_date, crop_task.get("end_day"))
 			})
 			task.insert()
+
+	def load_tasks(self):
+		"""Load `tasks` from the database"""
+		self.tasks = []
+		for task in self.get_tasks():
+			task_map = {
+				"title": task.subject,
+				"status": task.status,
+				"start_date": task.exp_start_date,
+				"end_date": task.exp_end_date,
+				"description": task.description,
+				"task_id": task.name,
+				"task_weight": task.task_weight
+			}
+
+			self.append("tasks", task_map)
+
+	def get_tasks(self):
+		if self.name is None:
+			return {}
+		else:
+			filters = {"crop_cycle": self.name}
+
+			if self.get("deleted_task_list"):
+				filters.update({
+					'name': ("not in", self.deleted_task_list)
+				})
+
+			return frappe.get_all("Task", "*", filters, order_by="exp_start_date asc")
+
+	def load_crop_inputs(self):
+		"""Load `crop inputs` from the database"""
+		self.crop_cycle_input_items = []
+		for crop_input in self.get_crop_inputs():
+			crop_inputs_map = {
+				"item_code": crop_input.item_code,
+				"item_name": crop_input.item_name,
+				"qty": (float(crop_input.qty) * float(self.area)),
+				"uom": crop_input.uom,
+				"valuation_rate": crop_input.valuation_rate,
+				"expected_harvest_date": add_days(self.start_date, crop_input.expected_harvest_start),
+				"expected_harvest_viability_date": add_days(self.start_date, crop_input.expected_harvest_end),
+				"source_warehouse": crop_input.source_warehouse,
+				"target_warehouse": crop_input.target_warehouse
+			}
+
+			self.append("crop_cycle_input_items", crop_inputs_map)
+
+	def get_crop_inputs(self):
+		if self.crop is None:
+			return {}
+		else:
+			filters = {"parent": self.crop}
+
+			# if self.get("deleted_task_list"):
+			# 	filters.update({
+			# 		'name': ("not in", self.deleted_task_list)
+			# 	})
+
+			return frappe.get_all("Crop Input Item", "*", filters, order_by="expected_harvest_start asc")
+
+	def load_crop_harvest_items(self):
+		"""Load `crop harvest items` from the database"""
+		self.crop_harvest_item_viability_window = []
+		for crop_harvest_item in self.get_crop_harvest_items():
+			crop_harvest_items_map = {
+				"item_code": crop_harvest_item.item_code_harvest,
+				"item_name": crop_harvest_item.item_name_harvest,
+				"qty": (float(crop_harvest_item.qty) * float(self.area)),
+				"uom": crop_harvest_item.uom,
+				"valuation_rate": crop_harvest_item.valuation_rate,
+				"expected_harvest_date": add_days(self.start_date, crop_harvest_item.expected_harvest_start),
+				"expected_harvest_viability_date": add_days(self.start_date, crop_harvest_item.expected_harvest_end),
+				"source_warehouse": crop_harvest_item.source_warehouse,
+				"target_warehouse": crop_harvest_item.target_warehouse
+			}
+
+			self.append("crop_harvest_item_viability_window", crop_harvest_items_map)
+
+	def get_crop_harvest_items(self):
+		if self.crop is None:
+			return {}
+		else:
+			filters = {"parent": self.crop}
+
+			# if self.get("deleted_task_list"):
+			# 	filters.update({
+			# 		'name': ("not in", self.deleted_task_list)
+			# 	})
+
+			return frappe.get_all("Crop Harvest Item", "*", filters, order_by="expected_harvest_start asc")
+
+	def delete_task(self):
+		if not self.get('deleted_task_list'): return
+
+		for d in self.get('deleted_task_list'):
+			frappe.delete_doc("Task", d)
+
+		self.deleted_task_list = []
 
 	def reload_linked_analysis(self):
 		linked_doctypes = ['Soil Texture', 'Soil Analysis', 'Plant Analysis']
@@ -126,3 +241,12 @@ def is_in_location(point, vs):
 		j += 1
 
 	return inside
+
+@frappe.whitelist()
+def create_kanban_board_if_not_exists(crop_cycle):
+	from frappe.desk.doctype.kanban_board.kanban_board import quick_kanban_board
+
+	if not frappe.db.exists('Kanban Board', crop_cycle):
+		quick_kanban_board('Task', crop_cycle, 'status')
+
+	return True
