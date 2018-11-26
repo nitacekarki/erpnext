3ce4b0ab5c17a901aa57390491be470d7ed7acc0
monroy95 <m.monroyc22@gmail.com>
2018-11-26 16:23:47 -0600

    Conversion Perennial date

--- erpnext/agriculture/doctype/crop/crop.js
+++ erpnext/agriculture/doctype/crop/crop.js
@@ -3,9 +3,40 @@
 
 frappe.provide("erpnext.crop");
 
+function calculate_crop_cycle_qty(frm) {
+	frappe.call({
+		method: 'erpnext.agriculture.doctype.crop.crop.convert_time',
+		args: {
+			time_uom: frm.doc.uom_date,
+			period_uom: frm.doc.date_uom_crop_cycle,
+			duration_crop_cycle: frm.doc.duration_of_crop_cycle,
+			crop_cycle_period: frm.doc.crop_cycle_period
+		},
+		callback: (r) => {
+			console.log(r.message);
+			cur_frm.set_value('estimated_crop_cycles', r.message);
+			refresh_field('estimated_crop_cycles');
+		}
+	});
+}
+
 frappe.ui.form.on('Crop', {
 	refresh: (frm) => {
-		frm.fields_dict.materials_required.grid.set_column_disp('bom_no', false);
+		// frm.fields_dict.materials_required.grid.set_column_disp('bom_no', false);
+		frappe.call({
+			method: "erpnext.agriculture.doctype.crop.crop.get_time_uom",
+			callback: function (r) {
+				// let time_unit_array = r.message;
+				frm.set_df_property("uom_date", "options", time_unit_array);
+				frm.set_df_property("date_uom_crop_cycle", "options", time_unit_array);
+			}
+		});
+	},
+	uom_date: (frm) => {
+		calculate_crop_cycle_qty(frm);
+	},
+	date_uom_crop_cycle: (frm) => {
+		calculate_crop_cycle_qty(frm);
 	}
 });
 
@@ -21,13 +52,13 @@ frappe.ui.form.on("BOM Item", {
 	}
 });
 
-erpnext.crop.update_item_rate_uom = function(frm, cdt, cdn) {
+erpnext.crop.update_item_rate_uom = function (frm, cdt, cdn) {
 	let material_list = ['materials_required', 'produce', 'byproducts'];
 	material_list.forEach((material) => {
 		frm.doc[material].forEach((item, index) => {
-			if (item.name == cdn && item.item_code){
+			if (item.name == cdn && item.item_code) {
 				frappe.call({
-					method:'erpnext.agriculture.doctype.crop.crop.get_item_details',
+					method: 'erpnext.agriculture.doctype.crop.crop.get_item_details',
 					args: {
 						item_code: item.item_code
 					},
@@ -41,11 +72,11 @@ erpnext.crop.update_item_rate_uom = function(frm, cdt, cdn) {
 	});
 };
 
-erpnext.crop.update_item_qty_amount = function(frm, cdt, cdn) {
+erpnext.crop.update_item_qty_amount = function (frm, cdt, cdn) {
 	let material_list = ['materials_required', 'produce', 'byproducts'];
 	material_list.forEach((material) => {
 		frm.doc[material].forEach((item, index) => {
-			if (item.name == cdn){
+			if (item.name == cdn) {
 				if (!frappe.model.get_value('BOM Item', item.name, 'qty'))
 					frappe.model.set_value('BOM Item', item.name, 'qty', 1);
 				frappe.model.set_value('BOM Item', item.name, 'amount', item.qty * item.rate);
@@ -53,3 +84,4 @@ erpnext.crop.update_item_qty_amount = function(frm, cdt, cdn) {
 		});
 	});
 };
+
--- erpnext/agriculture/doctype/crop/crop.py
+++ erpnext/agriculture/doctype/crop/crop.py
@@ -31,3 +31,46 @@ class Crop(Document):
 def get_item_details(item_code):
 	item = frappe.get_doc('Item', item_code)
 	return {"uom": item.stock_uom, "rate": item.valuation_rate}
+
+@frappe.whitelist()
+def get_time_uom():
+	prueba_data = frappe.db.get_values('UOM Conversion Factor',
+										filters={'category': 'Time'},
+										fieldname=['to_uom'])
+
+	# naming_series = frappe.get_meta("UOM Conversion Factor").get_field("to").options or ""
+	# prueba_data = prueba_data.split("\n")
+
+	return sorted(set(prueba_data))
+
+@frappe.whitelist()
+def convert_time(time_uom, period_uom, duration_crop_cycle, crop_cycle_period):
+
+	crop_cycle_duration_data = frappe.db.get_values('UOM Conversion Factor',
+									filters={'category': 'Time', 'to_uom': time_uom},
+									fieldname=['value', 'from_uom'], as_dict=True)
+
+	crop_cycle_period_data = frappe.db.get_values('UOM Conversion Factor',
+										filters={'category': 'Time', 'to_uom': period_uom},
+										fieldname=['value', 'from_uom'], as_dict=True)
+
+	if crop_cycle_duration_data[0]['from_uom']=='Second':
+		#If the conversion factor is to seconds, then execute the conversion.
+
+		crop_cycle_duration_seconds = float(duration_crop_cycle) / crop_cycle_duration_data[0]['value']
+		#frappe.msgprint(_("Crop Cycle duration in seconds: " + str(crop_cycle_duration_seconds)))
+
+		if crop_cycle_period_data[0]['from_uom']=='Second':
+			#crop_cycle_duration_seconds = float(duration_crop_cycle) * period_uom[0]['value']
+			crop_cycle_period_seconds = float(crop_cycle_period) / crop_cycle_period_data[0]['value']
+			#frappe.msgprint(_("Crop Cycle period in seconds: " + str(crop_cycle_period_seconds)))
+
+			number_of_crop_cycles = crop_cycle_duration_seconds / crop_cycle_period_seconds
+			#frappe.msgprint(_("Number of crop cycles should be " + str(number_of_crop_cycles)))
+			
+		else:
+			frappe.msgprint(_("Please add a conversion to seconds in UOM Conversion Factor for " + crop_cycle_period_data[0]['from_uom']))
+	else:
+		frappe.msgprint(_("Please add a conversion to seconds in UOM Conversion Factor " + crop_cycle_duration_data[0]['from_uom']))
+
+	return number_of_crop_cycles
